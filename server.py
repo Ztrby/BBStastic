a8f82a0d366f978335cb7d063ea20d36a7dc265b
Ztrby <163393333+Ztrby@users.noreply.github.com>
2024-07-15 21:56:12 +0200

    Create server.py
    From example at meshtastic.org

---
+++ server.py
@@ -0,0 +1,18 @@
+import time
+import meshtastic
+import meshtastic.tcp_interface
+from pubsub import pub
+
+def onReceive(packet, interface): # called when a packet arrives
+    print(f"Received: {packet}")
+
+def onConnection(interface, topic=pub.AUTO_TOPIC): # called when we (re)connect to the radio
+    # defaults to broadcast, specify a destination ID if you wish
+    #interface.sendText("hello mesh")
+
+pub.subscribe(onReceive, "meshtastic.receive")
+pub.subscribe(onConnection, "meshtastic.connection.established")
+interface = meshtastic.tcp_interface.TCPInterface(hostname='192.168.100.165')
+while True:
+    time.sleep(1000)
+interface.close()
