cf795f31d74c57052544e40dddefafcaf245c76e
Ztrby <163393333+Ztrby@users.noreply.github.com>
2024-07-15 22:18:48 +0200

    Update server.py
    nice way to exit application CTRL-C

--- server.py
+++ server.py
@@ -14,6 +14,11 @@ def onConnection(interface, topic=pub.AUTO_TOPIC): # called when we (re)connect 
 pub.subscribe(onReceive, "meshtastic.receive")
 pub.subscribe(onConnection, "meshtastic.connection.established")
 interface = meshtastic.tcp_interface.TCPInterface(hostname='192.168.100.165')
+
+try:
 while True:
     time.sleep(1000)
-interface.close()
+
+ except KeyboardInterrupt:
+        logging.info("Shutting down the server...")
+        interface.close()
