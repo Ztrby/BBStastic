1ef9f2f2d48b2eb043b569a6b5cb9735dce0749f
Ztrby <163393333+Ztrby@users.noreply.github.com>
2024-07-19 13:30:13 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,28 +1,79 @@
 import time
+import sys
+from urllib.request import urlopen 
+from socket import timeout
 import logging
 import meshtastic
 import meshtastic.serial_interface, meshtastic.tcp_interface
+import BBScommands
 from pubsub import pub
-
-def onReceive(packet, interface): # called when a packet arrives
-    print(f"Received: ")
-    logging.info(f"Packet received")
-
-def onConnection(interface, topic=pub.AUTO_TOPIC): # called when we (re)connect to the radio
-    print(f"Connected")
-
-pub.subscribe(onReceive, "meshtastic.receive")
-pub.subscribe(onConnection, "meshtastic.connection.established")
-
-# Create inteface
-interface = meshtastic.tcp_interface.TCPInterface(hostname='192.168.100.165')
-# interface = meshtastic.serial_interface.SerialInterface()
+import errno 
 
 
 try:
-    while True:
-        time.sleep(1000)
 
-except KeyboardInterrupt:
-    logging.info("Shutting down the server...")
-    interface.close()
+    if len(sys.argv) < 2:
+        print(f"usage: {sys.argv[0]} IP")
+        sys.exit(3)
+
+    def onReceive(packet, interface): # called when a packet arrives
+        BBScommands.readmessage(packet, interface)
+
+
+    def onConnection(interface, topic=pub.AUTO_TOPIC): # called when we (re)connect to the radio
+        print(f"Connected")
+
+    def onDisconnect(interface, topic=pub.AUTO_TOPIC):
+        print(f"Disconnected")
+        interface.close()
+        try:
+            interface = meshtastic.tcp_interface.TCPInterface(sys.argv[1])
+        except:
+            print(f"Error: Could not connect to {sys.argv[1]}")
+            sys.exit(1)
+
+    pub.subscribe(onReceive, "meshtastic.receive")
+    pub.subscribe(onConnection, "meshtastic.connection.established")
+    pub.subscribe(onDisconnect, "meshtastic.connection.lost")
+
+
+# Create inteface2
+    try:
+        interface = meshtastic.tcp_interface.TCPInterface(sys.argv[1])
+    except:
+        print(f"Error: Could not connect to {sys.argv[1]}")
+        sys.exit(1)
+
+    try:
+        while True:
+            time.sleep(1000)
+
+    except KeyboardInterrupt:
+        logging.info("Shutting down the server...")
+        print(f"\nShutting down the server...")
+        interface.close()
+    except IOError as e: 
+        if e.errno == errno.EPIPE: 
+            print(e.errno)
+            pass
+
+except IOError as e: 
+    if e.errno == errno.EPIPE: 
+      print(e.errno)
+      pass
+       # Handling of the error
+except ConnectionResetError:
+    print("==> ConnectionResetError")
+    pass
+except timeout: 
+    print("==> Timeout")
+    pass
+except BrokenPipeError:
+    print("==> BrokePipeerror")
+
+
+# interface = meshtastic.serial_interface.SerialInterface()
+
+
+
+
