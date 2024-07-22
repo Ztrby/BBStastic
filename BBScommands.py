d5b9e8a9e3bf710829c6b509de76acb9c6a05c2b
Ztrby <jonas.e.strom@gmail.com>
2024-07-23 00:33:29 +0200

    Update BBScommands.py

--- BBScommands.py
+++ BBScommands.py
@@ -1,3 +1,8 @@
+import BBSmenus
+import nodes
+import db_commands
+import time
+
 def readmessage(packet, interface):
     try:
         if 'decoded' in packet and packet['decoded']['portnum'] == 'TEXT_MESSAGE_APP':
@@ -6,41 +11,51 @@ def readmessage(packet, interface):
             meshfrom_hex_id = packet['fromId']
             sender_id = packet['from']
             to_id = packet.get('to')
+            # Update database
+            print("Textmessage resived")
+            #nodes.update_node(interface, meshfrom_hex_id)
 
             #Check that message is a direct message to BBS-node
-            if to_id==3662954260:
+            mynode=interface.getNode('^local')
+
+            if str(to_id)==str(mynode.nodeNum):
+                nodes.update_connected(interface, meshfrom_hex_id)
+                print("message: " + str(message_string) + " from " + str(meshfrom_hex_id))
                 match len(message_string):
                     # First level main menu3
                     case 1:
                         match message_string.upper():
                             case "M":
                                 print("Mailmenu")
-                                replay(mail_menu(), interface, meshfrom_hex_id)
+                                replay(BBSmenus.mail_menu(), interface, meshfrom_hex_id)
                             case "N":
                                 print("News menu")
-                                replay(news_menu(), interface, meshfrom_hex_id)
+                                replay(BBSmenus.news_menu(), interface, meshfrom_hex_id)
                             case "S":
                                 print("Stats menu")
-                                replay(stats_menu(), interface, meshfrom_hex_id)
+                                replay(BBSmenus.stats_menu(), interface, meshfrom_hex_id)
                             case "U":
                                 print("User menu")
-                                replay(user_menu(), interface, meshfrom_hex_id)
+                                replay(BBSmenus.user_menu(), interface, meshfrom_hex_id)
                             case "Q":
                                 print("Quick menu")
-                                replay(quick_menu(), interface, meshfrom_hex_id)
+                                replay(BBSmenus.quick_menu(), interface, meshfrom_hex_id)
                             case "H":
                                 print("Help menu")
-                                replay(help_menu(), interface, meshfrom_hex_id)
+                                replay(BBSmenus.help_menu(), interface, meshfrom_hex_id)
                             case _:
                                 print("mainmenu")
-                                replay(main_menu(), interface, meshfrom_hex_id)
+                                replay(BBSmenus.main_menu(), interface, meshfrom_hex_id)
                     case 2:
                         # Second level menu
                         match message_string.upper():
                         # Mail
                             # Mail read
                             case "MR":
-                                replay("Mail read, not implemented yet", interface, meshfrom_hex_id)
+                                if db_commands.check_exists("users", "primnode", meshfrom_hex_id):
+                                    replay("Mail read, not implemented yet", interface, meshfrom_hex_id)
+                                else:
+                                    replay("You have to create a user to read mail", interface, meshfrom_hex_id)
                             # Mail send
                             case "MS":
                                 replay("Mail send, not implemented yet", interface, meshfrom_hex_id)
@@ -60,14 +75,17 @@ def readmessage(packet, interface):
                         # Stats
                             # Stats nodes
                             case "SN":
-                                replay("Stats nodes, not implemented yet", interface, meshfrom_hex_id)
+                                replay(db_commands.statistics_nodes(), interface, meshfrom_hex_id)
                             # Stats Mesh
                             case "SM":
-                                replay("Stats mesh, not implemented yet", interface, meshfrom_hex_id)
+                                replay("There are: " + db_commands.statistic_mesh() + " known nodes in this mesh" , interface, meshfrom_hex_id)
                         # User menu
                             # User Create
                             case "UC":
-                                replay("User create, not implemented yet", interface, meshfrom_hex_id)
+                                if db_commands.check_exists("users", "primnode", meshfrom_hex_id):
+                                    replay("You are already a user in the BBS", interface,meshfrom_hex_id)
+                                else:
+                                    replay("User crete not implementet yet", interface, meshfrom_hex_id)
                             # User Update
                             case "UU":
                                 replay("User update, not implemented yet", interface, meshfrom_hex_id)
@@ -75,69 +93,59 @@ def readmessage(packet, interface):
                             case "UD":
                                 replay("User Delete, not implemented yet", interface, meshfrom_hex_id)
                             case _:
-                                replay(main_menu(), interface, meshfrom_hex_id)
+                                print("mainmenu")
+                                replay(BBSmenus.main_menu(), interface, meshfrom_hex_id)
                     case 4:
                         # Second level menu
                         match message_string.upper():
                             case "HELP":
                                 print("Help menu")
-                                replay(help_menu(), interface, meshfrom_hex_id)
+                                replay(BBSmenus.help_menu(), interface, meshfrom_hex_id)
                             case _:
-                                replay(main_menu(), interface, meshfrom_hex_id) 
+                                replay(BBSmenus.main_menu(), interface, meshfrom_hex_id) 
                     # Protecting from autoreplay ping pong        
                     case _ if len(message_string) < 5:
                         print("under 5 char sent")
-                        replay(main_menu(), interface, meshfrom_hex_id)         
+                        replay(BBSmenus.main_menu(), interface, meshfrom_hex_id)         
 
                     case _:
                         print(f"Received: {message_string} from {meshfrom_hex_id} , {sender_id} , {to_id}")
             else:
                  print(f"Not sent to BBS-node")
+        # NODEINFO_APP
+        if 'decoded' in packet and packet['decoded']['portnum'] == 'NODEINFO_APP':
+            meshfrom_hex_id = packet['fromId']
+            if meshfrom_hex_id:
+                nodes.update_node_packet_nodeapp(interface, meshfrom_hex_id,packet)
+        # TETELEMETRY_APP
+        if 'decoded' in packet and packet['decoded']['portnum'] == 'TELEMETRY_APP':
+            meshfrom_hex_id = packet['fromId']
+            nodes.update_node_packet_teleapp(interface, meshfrom_hex_id,packet)
+        # POSITION_APP
+        if 'decoded' in packet and packet['decoded']['portnum'] == 'POSITION_APP':
+            meshfrom_hex_id = packet['fromId']
+            nodes.update_node_packet_posapp(interface, meshfrom_hex_id,packet)
+        if 'decoded' in packet and packet['decoded']['portnum'] == 'NEIGHBORINFO_APP':
+            meshfrom_hex_id = packet['fromId']
+            print("NEIGHBORINFO recived from " + meshfrom_hex_id)
+            nodes.update_node_packet_neighborapp(interface, meshfrom_hex_id,packet)
     except KeyError as e:
             print(f"Error processing packet: {e}")
 
+
 def replay(answer_string, interface, dest):
-    interface.sendText(\
-    text=answer_string, \
-    destinationId=dest, \
-    wantAck=False, \
-    wantResponse=False)
-    
-    
-    
-def get_node_short_name(node_id, interface):
-    node_info = interface.nodes.get(node_id)
-    if node_info:
-        return node_info['user']['shortName']
-    return None
+    max_size = 180
+    for i in range(0, len(answer_string), max_size):
+        piece = answer_string[i:i + max_size]
+        interface.sendText(
+            text=piece,
+            destinationId=dest,
+            wantAck=False,
+            wantResponse=False
+        )
+        time.sleep(5)
 
-def idToHex(nodeId):
-    return '!' + hex(nodeId)[2:]
 
-def main_menu():
-    menu_string = f"***** Main menu ******\n[M]ail\n[N]ews\n[S]tats\n[U]ser\n[Q]uick commands\n[H]elp"
-    return menu_string
+""" def idToHex(nodeId):
+    return '!' + hex(nodeId)[2:] """
 
-def mail_menu():
-    menu_string = f"***** Mail menu ******\n[MR] Read\n[MS] Send\n[MD] Delete"
-    return menu_string
-
-def news_menu():
-    menu_string = f"***** News menu ******\n[NR] Read\n[NP] Post\n[ND] Delete"
-    return menu_string
-
-def stats_menu():
-    menu_string = f"***** Stats menu ******\n[SN] Nodes\n[SM] Mesh"
-    return menu_string
-
-def user_menu():
-    menu_string = f"***** User menu ******\n[UC] Create user\n[UU] Update user\n[UD] User delete"
-    return menu_string
-
-def quick_menu():
-    menu_string = f"***** Quick menu ******\nMS!!shortname!!subject!!text\n"
-    return menu_string
-
-def help_menu():
-    menu_string = f"***** Help menu ******\nGo to link to see onlinehelp\nhttps://github.com/Ztrby/BBStastic"
-    return menu_string
